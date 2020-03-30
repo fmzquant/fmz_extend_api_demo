2c7ee852962260493ed2345abe28708d02eab1d7
Zero <zeropool@qq.com>
2020-03-30 17:01:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -315,4 +315,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=False, threaded=True)
+    app.run(debug=False, threaded=True, host='127.0.0.1', port=5000)
