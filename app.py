261a44ec23a201a6e8c25944922ce260ebd76e35
litao <litao1@chinaexpress.com>
2020-06-04 14:42:25 +0800

    change access_key, secret_key to config.py

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 #!encoding=utf8
 
 
-import os, time, logging, json, sqlite3, datetime
+import datetime
+import json
+import logging
+import os
+import time
+
 try:
     import md5
     import urllib2
     from urllib import urlencode
-except:
+except Exception as e:
     import hashlib as md5
     import urllib.request as urllib2
     from urllib.parse import urlencode
 from flask import jsonify, request, Flask, render_template, redirect, url_for
-from flask_bootstrap import WebCDN, Bootstrap
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [line:%(lineno)d] %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+from config import BOTVS_ACCESS_KEY, BOTVS_SECRET_KEY
 
-# 配置BotVS平台的AccessKey与SecretKey (修改xxxxx/yyyyy)
-BOTVS_ACCESS_KEY = os.getenv('BOTVS_ACCESS_KEY', 'xxxxx')
-BOTVS_SECRET_KEY = os.getenv('BOTVS_SECRET_KEY', 'yyyyy')
-
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 class cached(object):
     def __init__(self, *args, **kwargs):
         self.kv = {}
         self.timeout = kwargs.get("timeout", 0)
+
     def __call__(self, func):
         def inner(*args, **kwargs):
             if not kwargs.get('cache', False):
@@ -40,7 +44,9 @@ class cached(object):
                 res = func(*args, **kwargs)
                 self.kv[k] = {'data': res, 'fetch_time': now}
             return self.kv[k]['data']
+
         return inner
+
 
 @cached(timeout=600)
 def api(method, *args):
@@ -50,31 +56,40 @@ def api(method, *args):
         'method': method,
         'args': json.dumps(list(args)),
         'nonce': int(time.time() * 1000),
-        }
-    d['sign'] = md5.md5(('%s|%s|%s|%d|%s' % (d['version'], d['method'], d['args'], d['nonce'], BOTVS_SECRET_KEY)).encode('utf-8')).hexdigest()
-    return json.loads(urllib2.urlopen('https://www.fmz.com/api/v1', urlencode(d).encode('utf-8')).read().decode('utf-8'))
+    }
+    d['sign'] = md5.md5(
+        ('%s|%s|%s|%d|%s' % (d['version'], d['method'], d['args'], d['nonce'], BOTVS_SECRET_KEY)).encode(
+            'utf-8')).hexdigest()
+    return json.loads(
+        urllib2.urlopen('https://www.fmz.com/api/v1', urlencode(d).encode('utf-8')).read().decode('utf-8'))
 
 
 exchanges_list = None
+
+
 def get_exchange_list(force=False):
     global exchanges_list
     if exchanges_list is None or force:
         exchanges_list = json.loads(urllib2.urlopen('https://www.fmz.com/chart/symbols.json').read())
-        logging.debug(' * Initialize %d exchanges' % (len(exchanges_list), ))
+        logging.debug(' * Initialize %d exchanges' % (len(exchanges_list),))
     return exchanges_list
+
 
 def get_default_stock(eid):
     for e in get_exchange_list():
         if e['eid'] == eid:
             return e['stocks'].split(',')[0]
 
+
 def plugin_run(exchanges, code, pair=None, period=900):
-    settings = { "period": period/60, "source": code, "exchanges": []}
+    settings = {"period": period / 60, "source": code, "exchanges": []}
     for e in exchanges:
         if pair is None:
             pair = get_default_stock(e.eid)
-        settings["exchanges"].append({"eid": e.eid, "pair": pair, "meta" :{"AccessKey": e.accessKey, "SecretKey": e.secretKey}})
+        settings["exchanges"].append(
+            {"eid": e.eid, "pair": pair, "meta": {"AccessKey": e.accessKey, "SecretKey": e.secretKey}})
     return api('PluginRun', settings)
+
 
 def robot_run(robotId, appId, exchanges):
     strategyId = -1
@@ -83,17 +98,18 @@ def robot_run(robotId, appId, exchanges):
         if 'main' in ele['name']:
             strategyId = ele['id']
     if strategyId < 0:
-        raise u"not found strategy"
+        raise Exception(u"not found strategy")
     settings = {
-            "name":"robot for %s" % (appId, ),
-            "args": [], # our custom arguments for this strategey
-            "appid": appId, # 为该机器人设置标签,关联到本用户
-            "period": 60,
-            "strategy": strategyId,
-            "exchanges": [],
-            }
+        "name": "robot for %s" % (appId,),
+        "args": [],  # our custom arguments for this strategey
+        "appid": appId,  # 为该机器人设置标签,关联到本用户
+        "period": 60,
+        "strategy": strategyId,
+        "exchanges": [],
+    }
     for e in exchanges:
-        settings["exchanges"].append({"eid": e.eid, "pair": get_default_stock(e.eid), "meta" :{"AccessKey": e.accessKey, "SecretKey": e.secretKey}})
+        settings["exchanges"].append({"eid": e.eid, "pair": get_default_stock(e.eid),
+                                      "meta": {"AccessKey": e.accessKey, "SecretKey": e.secretKey}})
     if robotId > 0:
         return api('RestartRobot', robotId, settings)
     else:
@@ -102,11 +118,11 @@ def robot_run(robotId, appId, exchanges):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supposedtobeveryimportantsecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'user.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                                    'user.sqlite3')
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 bootstrap = Bootstrap(app)
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -114,12 +130,14 @@ login_manager.login_view = 'login'
 
 db = SQLAlchemy(app)
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
     date = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
 
 class Exchange(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -130,29 +148,36 @@ class Exchange(db.Model):
     secretKey = db.Column(db.Text)
     date = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
 
+
 db.create_all()
 db.session.commit()
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class LoginForm(FlaskForm):
     username = StringField(u'用户名', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField(u'密码', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 class RegisterForm(FlaskForm):
     email = StringField(u'邮箱', validators=[InputRequired(), Email(message='无效的Email'), Length(max=50)])
     username = StringField(u'用户名', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField(u'密码', validators=[InputRequired(), Length(min=8, max=80)])
 
+
 @app.route('/')
 def index():
     return render_template('index.html', current_user=current_user)
 
+
 @app.route('/update', methods=['GET', 'POST'])
 def update():
     return jsonify(get_exchange_list(True))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -164,17 +189,18 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
-            if user.password == md5.md5((user.email+'__slat__'+form.password.data).encode("utf8")).hexdigest():
+            if user.password == md5.md5((user.email + '__slat__' + form.password.data).encode("utf8")).hexdigest():
                 login_user(user)
                 return redirect(url_for('dashboard'))
         error = u'用户名或密码错误'
     return render_template('login.html', current_user=current_user, form=form, error=error)
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
     if form.validate_on_submit():
-        hashed_password = md5.md5((form.email.data+'__slat__'+form.password.data).encode("utf8")).hexdigest()
+        hashed_password = md5.md5((form.email.data + '__slat__' + form.password.data).encode("utf8")).hexdigest()
         ele = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(ele)
         db.session.commit()
@@ -183,10 +209,11 @@ def signup():
 
     return render_template('signup.html', current_user=current_user, form=form)
 
+
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    appId = "appId_%d" % (current_user.id, )
+    appId = "appId_%d" % (current_user.id,)
     # 通过标签或者对应的该用户所有的运行的机器人
     robots = api('GetRobotList', appId)['data']['result']['robots']
     robotId = -1
@@ -205,7 +232,7 @@ def dashboard():
         if action:
             if action == "refresh":
                 # 刷新收益
-                return jsonify({'profit':profit, 'running': isRunning})
+                return jsonify({'profit': profit, 'running': isRunning})
             elif action == "run":
                 # 运行策略
                 result = robot_run(robotId, appId, Exchange.query.filter_by(user_id=current_user.id).all())
@@ -218,7 +245,9 @@ def dashboard():
             return jsonify(result)
 
     platforms = Exchange.query.filter_by(user_id=current_user.id).all()
-    return render_template('dashboard.html', current_user=current_user, platforms=platforms, running=isRunning, profit=profit)
+    return render_template('dashboard.html', current_user=current_user, platforms=platforms, running=isRunning,
+                           profit=profit)
+
 
 @app.route('/exchanges', methods=['GET', 'POST'])
 @login_required
@@ -237,6 +266,7 @@ def exchanges():
         error = u"表格填写不完整"
     return render_template('dashboard.html', current_user=current_user, exchanges=get_exchange_list(), error=error)
 
+
 @app.route('/assets', methods=['GET', 'POST'])
 @login_required
 def assets():
@@ -248,6 +278,7 @@ def assets():
             return jsonify(results=True)
     platforms = Exchange.query.filter_by(user_id=current_user.id).all()
     return render_template('dashboard.html', current_user=current_user, platforms=platforms)
+
 
 @app.route('/hub', methods=['GET', 'POST'])
 @login_required
@@ -304,15 +335,18 @@ def hub():
     for ele in platforms:
         for obj in es:
             if obj['eid'] == ele.eid:
-                arr.append({'id': ele.id, 'pid': ele.id, 'name': obj['name'], 'symbols': obj['symbols'], 'eid': ele.eid, 'label': ele.label})
+                arr.append({'id': ele.id, 'pid': ele.id, 'name': obj['name'], 'symbols': obj['symbols'], 'eid': ele.eid,
+                            'label': ele.label})
                 break
     return render_template('dashboard.html', current_user=current_user, platforms=json.dumps(arr))
+
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=False, threaded=True, host='127.0.0.1', port=5000)
---
+++ config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+
+import os
+
+# 配置BotVS平台的AccessKey与SecretKey (修改xxxxx/yyyyy)
+BOTVS_ACCESS_KEY = os.getenv('BOTVS_ACCESS_KEY', 'xxxxx')
+BOTVS_SECRET_KEY = os.getenv('BOTVS_SECRET_KEY', 'yyyyy')
